308a1d53f2069cc29084e0bd287fe8b3ab0d6f1f
IGOR-PC\J.Sound <igor2d@mail.ru>
2021-10-20 20:18:34 +0300

    no message

---
+++ Shop/Shop/shop.data/ShopDbcontext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Shop.Core;
+using Shop.Core.Domain;
+
+namespace shop.data
+{
+    public class ShopDbcontext : IdentityDbContext
+    {
+        public ShopDbcontext(DbContextOptions<ShopDbcontext> options)
+            : base(options) { }           
+    public DbSet<Product> Product { get; set; }
+    }
+}
